e7442c7b2e18013d2541d98888786344c60d2b91
Rabiul506 <118612711+Rabiul506@users.noreply.github.com>
2024-10-12 09:55:12 +0600

    Email card projects are added
    In the Email card project .There are 3 work together.
    1.Email card main
    2.Login card
    3.Galery picture work

---
+++ Email_card/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        Roboto:"Roboto, sans-serif"
+      },
+
+      screens:{
+
+        'sm': '430px',
+        'md': '768px',
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
+  },
+  plugins: [],
+}
+
